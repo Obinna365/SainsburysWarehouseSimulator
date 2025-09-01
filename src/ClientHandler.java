cc1e9331524cfcf3e4a0b7a7e9c4f1edfaedaad3
Obinna Adieze <93251781+Obinna365@users.noreply.github.com>
2025-09-01 18:58:10 +0100

    On the clientHandler I generated pallets

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.*;
 
 public class ClientHandler implements Runnable{
 
@@ -31,4 +32,33 @@ public class ClientHandler implements Runnable{
 
 
     }
+    private List<HashMap<String, Integer>> generatePallets(){
+        ArrayList<String> Goods = GenerateGoodsIn();
+        List<HashMap<String, Integer>> pallet = new ArrayList<>();
+        for (int i = 0; i < 20; i++){
+            int amount = (int)(Math.random() * 150) + 1;
+            String item = Goods.get(new Random().nextInt(Goods.size()));
+            HashMap<String, Integer> Product = new HashMap<>();
+            Product.put(item,amount);
+            pallet.add(Product);
+        }
+        return pallet;
+
+    }
+
+    private ArrayList<String> GenerateGoodsIn(){
+        ArrayList<String> food = new ArrayList<>();
+        food.add("Yogurt");
+        food.add("Bread");
+        food.add("Bagels");
+        food.add("Ready Meal: Pasta Bolognese");
+        food.add("Ready Meal: Fish and Chips");
+        food.add("Ready Meal: Chicken Pie");
+        food.add("Ready Meal: Toad in the hole");
+        food.add("Vienese Biscuits");
+        food.add("Cranberry Pie");
+        return food;
+
+    }
 }
+
